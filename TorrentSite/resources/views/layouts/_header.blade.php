02ecca1b13e327e2aee4bb292922ff2ff32d96bb
Simon Arpad <simonarpad2002@gmail.com>
2022-06-15 04:52:23 +0200

    _header update

--- TorrentSite/resources/views/layouts/_header.blade.php
+++ TorrentSite/resources/views/layouts/_header.blade.php
@@ -8,27 +8,22 @@
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li>
                     <div class="btn-group">
-                        <a type="button" class="btn btn-success" href="#">{{__(('Download'))}}</a>
+                        <a type="button" class="btn btn-success" href="{{route('download')}}">{{__(('Download'))}}</a>
                         <button type="button" class="btn btn-success dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
                             <span class="visually-hidden">Toggle Dropdown</span>
                         </button>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
+                            @foreach($categories as $category)
+                                <li><a class="dropdown-item" href="#">{{$category->title}}</a></li>
+                            @endforeach
                         </ul>
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{route('torrent.create')}}">Upload</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link disabled">Upload</a>
                 </li>
             </ul>
             <div>
